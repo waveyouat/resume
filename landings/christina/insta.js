aa04da57e38111e4ed6abd52e97654ee2b698221
T.L <waveyouat@gmail.com>
2020-05-25 22:32:02 +0100

    Instafeed in js

---
+++ landings/christina/insta.js
@@ -0,0 +1,15 @@
+new InstagramFeed({
+    'username': 'beingchristinal',
+    'container': "#instafeed",
+    'display_profile': true,
+    'display_biography': true,
+    'display_gallery': true,
+    'display_igtv': false,
+    'items': 10,
+    'items_per_row': 2,
+    'margin': 0.5,
+    'image_size': 480,
+    // 'get_raw_json': true,
+    // 'callback': function(data) {console.log(data)},
+    //'styling': false
+});
